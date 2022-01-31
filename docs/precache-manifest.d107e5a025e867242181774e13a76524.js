4f43caa413b7daddb00f9b0dd8b40b50c44a7be8
carlos260800 <mash.x2003@gmail.com>
2022-01-31 15:56:37 -0600

    dosc agregado

---
+++ docs/precache-manifest.d107e5a025e867242181774e13a76524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db4ec45650ab3aee5f67276ab6f9dee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "643dd8f7b04f70402c85",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "643dd8f7b04f70402c85",
+    "url": "/static/js/main.179cbd49.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
